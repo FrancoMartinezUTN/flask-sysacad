0b94b9926a3110b35ae1a07219353480984c8ca1
FrancoMartinezUTN <33930055@alumnos.frsr.utn.edu.ar>
2026-01-27 18:46:06 -0300

    ci: fix testing DB config and make CI pass

--- test/test_app.py
+++ test/test_app.py
@@ -1,21 +1,32 @@
-import unittest
-from flask import current_app
-from app import create_app
 import os
+import unittest
+
+from flask import current_app
+
+from app import create_app
+
 
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
+        # Configurar entorno para testing sin depender de .env
         os.environ['FLASK_CONTEXT'] = 'testing'
+        os.environ['FLASK_SKIP_DOTENV'] = '1'
+        
         self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
 
     def tearDown(self):
         self.app_context.pop()
+        # Limpiar env vars de testing
+        for var in ('FLASK_CONTEXT', 'FLASK_SKIP_DOTENV'):
+            os.environ.pop(var, None)
 
     def test_app(self):
+        """Verifica que la app se crea correctamente y current_app existe."""
         self.assertIsNotNone(current_app)
+
 
 if __name__ == '__main__':
     unittest.main()
